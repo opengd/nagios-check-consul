0c1867dfeedab30311e85cbb6a3cd6c0222206f5
opengd <opengd@outlook.com>
2018-09-06 12:28:49 +0200

    Added check that jq is installed

--- check_consul.sh
+++ check_consul.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 #COMMAND="check_consul $1"
 
+if [ -z "$(command -v jq)" ]
+then
+        echo "UNKNOWN - jq is not installed"
+        exit 3
+fi
+
 aa=$(curl -s $1)
 
 if [ -z "$aa" ] || [ "$aa" == "[]" ]
 then
         MESSAGE="CRITICAL - Could not retrieve json data from url"
-	STATUS=2
+        STATUS=2
 else
-	curl -s $1 | jq '.[].Checks[0] | "\(.Status) \(.Node)"' | awk '{rows++} {print $0} $0~"passing" {passing++} END {if(passing==rows) {exit 0} else {exit 2}}'
-	STATUS=$?
-	if [ $STATUS -eq 0 ]
-	then
-		MESSAGE="OK - all is passing"
-	else
-		MESSAGE="CRITICAL - check any not passing consuls"
-	fi
-	
+        curl -s $1 | jq '.[].Checks[0] | "\(.Status) \(.Node)"' | awk '{rows++} {print $0} $0~"passing" {passing++} END {if(passing==rows) {exit 0} else {exit 2}}'
+        STATUS=$?
+        if [ $STATUS -eq 0 ]
+        then
+                MESSAGE="OK - all is passing"
+        else
+                MESSAGE="CRITICAL - check any not passing consuls"
+        fi
+
 fi
 
 printf "$MESSAGE"
